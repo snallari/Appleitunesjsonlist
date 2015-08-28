bc14a25736e56ad4a39a3ddff3c8f6b77f248743
snallari <shruti.nallari@gamil.com>
2015-08-28 14:48:39 -0400

    apple itunes

---
+++ InterviewTest/app/src/main/java/com/intelematics/interview/tasks/DownloadSongListTask.java
@@ -0,0 +1,51 @@
+package com.intelematics.interview.tasks;
+
+import java.util.ArrayList;
+
+import com.intelematics.interview.R;
+import com.intelematics.interview.SongListActivity;
+import com.intelematics.interview.db.DBManager;
+import com.intelematics.interview.db.SongManager;
+import com.intelematics.interview.models.Song;
+import com.intelematics.interview.net.ConnectionManager;
+import com.intelematics.interview.util.JsonParser;
+import android.os.AsyncTask;
+
+/**
+ *
+ */
+public class DownloadSongListTask extends AsyncTask<Void, Void, Void> {
+	private DBManager dbManager;
+	private SongListActivity activity;
+	private ArrayList<Song> songList;
+	
+	private ConnectionManager connectionManager;
+	
+	public DownloadSongListTask(SongListActivity activity, DBManager dbManager) {
+		this.activity = activity;
+		this.dbManager = dbManager;
+		songList = new ArrayList<Song>();
+	}
+	
+	@Override
+	protected Void doInBackground(Void... params) {
+		JsonParser parser = new JsonParser();
+
+        connectionManager = new ConnectionManager(activity, "https://itunes.apple.com/search?term=tom+waits");
+
+		songList = parser.parseSongList(connectionManager.requestJson());
+		connectionManager.closeConnection();
+		
+		SongManager songManager = new SongManager(activity, dbManager);
+		songManager.saveSongsList(songList);
+		
+		return null;
+	}
+
+    protected void onPostExecute(Void result) {
+    	activity.updateSongList(songList);
+    }
+
+
+
+}
